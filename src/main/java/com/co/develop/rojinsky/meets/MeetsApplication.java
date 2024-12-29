98244e403ed83d66d177fd97e18562cb7d2701ba
rojonovich492 <haprorold@gmail.com>
2024-12-29 16:49:06 -0500

    Creation project meets

---
+++ src/main/java/com/co/develop/rojinsky/meets/MeetsApplication.java
@@ -0,0 +1,13 @@
+package com.co.develop.rojinsky.meets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/develop/rojinsky/meets/MeetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.develop.rojinsky.meets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
